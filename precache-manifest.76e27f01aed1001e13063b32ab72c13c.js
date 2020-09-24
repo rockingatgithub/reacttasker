3c01b333481b1914588239c63c49973648d1ce0b
rockingatgithub <contactwithsid@gmail.com>
2020-09-25 02:05:39 +0530

    Updates

---
+++ precache-manifest.76e27f01aed1001e13063b32ab72c13c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58d317044b0ab56e3e7e1d5b2a96bf74",
+    "url": "/reacttasker/index.html"
+  },
+  {
+    "revision": "742bc45abc3648698ab1",
+    "url": "/reacttasker/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4a8d572edf5c2d006fbb",
+    "url": "/reacttasker/static/js/2.feb06662.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/reacttasker/static/js/2.feb06662.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "742bc45abc3648698ab1",
+    "url": "/reacttasker/static/js/main.da25d6f6.chunk.js"
+  },
+  {
+    "revision": "82d0faecfc5242f230d2",
+    "url": "/reacttasker/static/js/runtime-main.8d123c94.js"
+  }
+]);
